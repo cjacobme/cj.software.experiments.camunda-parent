55483f8d7623c44c6bb8344c3f005f99606b9491
cjacobme <cjacobme@aol.com>
2019-11-08 22:11:28 +0100

    some minor modifications

--- CJ  -  Experiments  -  Camunda Parent/02-timer/src/main/java/cj/software/experiments/camunda/_02_timer/TimerLogger.java
+++ CJ  -  Experiments  -  Camunda Parent/02-timer/src/main/java/cj/software/experiments/camunda/_02_timer/TimerLogger.java
@@ -1,5 +1,6 @@
 package cj.software.experiments.camunda._02_timer;
 
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
@@ -13,11 +14,21 @@ public class TimerLogger
 {
 	private Logger logger = LoggerFactory.getLogger(TimerLogger.class);
 
+	private Random random = new Random();
+
 	@Override
 	public void execute(DelegateExecution pExecution) throws Exception
 	{
-		this.logger.info("Timer was triggered, sleeping now...");
-		TimeUnit.SECONDS.sleep(13);
+		double lNextRand = this.random.nextDouble();
+		long lSleepTime = (lNextRand > 0.75 ? 13 : 4);
+		this.logger
+				.info(
+						String
+								.format(
+										"Timer was triggered, %5.3f sleeping now for %d seconds...",
+										lNextRand,
+										lSleepTime));
+		TimeUnit.SECONDS.sleep(lSleepTime);
 		this.logger.info("woke up again");
 	}
 
