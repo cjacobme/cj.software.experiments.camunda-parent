85a13150b02d68bc18ad702d4f33d9137aa8887d
cjacobme <cjacobme@aol.com>
2020-01-21 21:45:30 +0100

    setup new project

---
+++ CJ  -  Experiments  -  Camunda Parent/07-hierarchical-decision/src/main/java/cj/software/experiments/camunda/_07_hierarchical/delegate/SimulateCheck.java
@@ -0,0 +1,18 @@
+package cj.software.experiments.camunda._07_hierarchical.delegate;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+public class SimulateCheck
+		implements
+		JavaDelegate
+{
+
+	@Override
+	public void execute(DelegateExecution pExecution) throws Exception
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
