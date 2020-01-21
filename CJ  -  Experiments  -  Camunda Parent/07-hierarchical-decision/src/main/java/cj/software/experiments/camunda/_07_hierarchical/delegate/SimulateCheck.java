6c1f853c37a14f9f9ecd7a50155a9747ed2170f6
cjacobme <cjacobme@aol.com>
2020-01-21 22:17:06 +0100

    log some variables

--- CJ  -  Experiments  -  Camunda Parent/07-hierarchical-decision/src/main/java/cj/software/experiments/camunda/_07_hierarchical/delegate/SimulateCheck.java
+++ CJ  -  Experiments  -  Camunda Parent/07-hierarchical-decision/src/main/java/cj/software/experiments/camunda/_07_hierarchical/delegate/SimulateCheck.java
@@ -1,18 +1,42 @@
 package cj.software.experiments.camunda._07_hierarchical.delegate;
 
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
+@Component
 public class SimulateCheck
 		implements
 		JavaDelegate
 {
+	private Logger logger = LoggerFactory.getLogger(SimulateCheck.class);
+
+	private Random random = new Random();
 
 	@Override
 	public void execute(DelegateExecution pExecution) throws Exception
 	{
-		// TODO Auto-generated method stub
+		String lCorrelationId = pExecution.getProcessInstanceId();
+		String lSystem = (String) pExecution.getVariable("System");
+		String lScope = (String) pExecution.getVariable("Scope");
+		String lApplication = (String) pExecution.getVariable("Application");
+		String lCheck = (String) pExecution.getVariable("Check");
 
+		this.logger.info(
+				"{}: called for system {} scope {} application {} check {}",
+				lCorrelationId,
+				lSystem,
+				lScope,
+				lApplication,
+				lCheck);
+
+		int lSleepSeconds = this.random.nextInt(7);
+		TimeUnit.SECONDS.sleep(lSleepSeconds);
 	}
 
 }
