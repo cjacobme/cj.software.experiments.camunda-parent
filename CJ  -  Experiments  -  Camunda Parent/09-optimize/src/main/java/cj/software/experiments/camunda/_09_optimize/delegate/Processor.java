847977b9d703c0706f3aef90acb1a48d03861654
cjacobme <cjacobme@aol.com>
2020-02-02 19:30:28 +0100

    working example to produce a sample for correlations

--- CJ  -  Experiments  -  Camunda Parent/09-optimize/src/main/java/cj/software/experiments/camunda/_09_optimize/delegate/Processor.java
+++ CJ  -  Experiments  -  Camunda Parent/09-optimize/src/main/java/cj/software/experiments/camunda/_09_optimize/delegate/Processor.java
@@ -1,6 +1,7 @@
 package cj.software.experiments.camunda._09_optimize.delegate;
 
-import java.util.concurrent.TimeUnit;
+import java.util.Objects;
+import java.util.Random;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -15,20 +16,28 @@ public class Processor
 {
 	private Logger logger = LogManager.getFormatterLogger();
 
+	private Random randomizer = new Random();
+
 	@Override
 	public void execute(DelegateExecution pExecution) throws Exception
 	{
 		String lCorrelationId = pExecution.getProcessInstanceId();
-		String lDepartment = (String) pExecution.getVariable("department");
-		String lDurationStr = (String) pExecution.getVariable("duration");
-		Integer lDuration = Integer.parseInt(lDurationStr);
+		String lDepartment = Objects.requireNonNull((String) pExecution.getVariable("department"));
+		String lBiasStr = Objects.requireNonNull((String) pExecution.getVariable("bias"));
+		int lBias = Integer.parseInt(lBiasStr);
+		String lRandomStr = Objects.requireNonNull((String) pExecution.getVariable("random"));
+		int lRandom = Integer.parseInt(lRandomStr);
 		this.logger.info(
-				"%s: department %s now sleep for %d seconds",
+				"%s: department=%s, bias=%d, random=%d",
 				lCorrelationId,
 				lDepartment,
-				lDuration);
-		TimeUnit.SECONDS.sleep(lDuration);
-		this.logger.info("%s: department %s woke up again", lCorrelationId, lDepartment);
+				lBias,
+				lRandom);
+
+		int lNextRandom = this.randomizer.nextInt(lRandom);
+		int lCosts = lBias + lNextRandom;
+		this.logger.info("%s: department %s had costs=%d", lCorrelationId, lDepartment, lCosts);
+		pExecution.setVariable("costs", lCosts);
 	}
 
 }
