7f58f6b28ffdeb2340d055f9eeb675d124e8b04d
cjacobme <cjacobme@aol.com>
2020-02-20 22:30:52 +0100

    task-id mit in Link

--- CJ  -  Experiments  -  Camunda Parent/10-send-mail/src/main/java/cj/software/experiments/camunda/_10_mail/delegate/MailSenderDelegate.java
+++ CJ  -  Experiments  -  Camunda Parent/10-send-mail/src/main/java/cj/software/experiments/camunda/_10_mail/delegate/MailSenderDelegate.java
@@ -3,8 +3,12 @@ package cj.software.experiments.camunda._10_mail.delegate;
 import java.net.URL;
 import java.util.Objects;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.camunda.bpm.engine.TaskService;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.bpm.engine.task.Task;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -20,7 +24,12 @@ public class MailSenderDelegate
 	private MyMailSender myMailSender;
 
 	@Autowired
+	private TaskService taskService;
+
+	@Autowired
 	private ConfigurationHolder configurationHolder;
+
+	private Logger logger = LogManager.getFormatterLogger();
 
 	@Override
 	public void execute(DelegateExecution pExecution) throws Exception
@@ -29,24 +38,38 @@ public class MailSenderDelegate
 		String lTo = Objects.requireNonNull((String) pExecution.getVariable("To"));
 		String lSubject = Objects.requireNonNull((String) pExecution.getVariable("Subject"));
 		String lBody = Objects.requireNonNull((String) pExecution.getVariable("Body"));
-		lBody = this.extendBody(lBody);
+		lBody = this.extendBody(pExecution, lBody);
 		this.myMailSender.sendMail(lFrom, lTo, lSubject, lBody);
 	}
 
-	private String extendBody(String pSource)
+	private String extendBody(DelegateExecution pExecution, String pSource)
 	{
 		StringBuilder lSB = new StringBuilder(pSource).append("\n\n\n");
 		lSB.append("please visit the following URL: ");
-		lSB.append(this.constructPath());
+		lSB.append(this.constructPath(pExecution));
 		return lSB.toString();
 	}
 
-	private String constructPath()
+	private String constructPath(DelegateExecution pExecution)
 	{
 		URL lBaseUrl = this.configurationHolder.getAddtlUrl();
 		String lResult = lBaseUrl
 				+ "app/tasklist/default/#/"
 				+ "?sorting=%5B%7B%22sortBy%22:%22priority%22,%22sortOrder%22:%22desc%22%7D%5D";
+		String lProcessInstanceId = pExecution.getProcessInstanceId();
+		Task lTask = this.taskService.createTaskQuery()
+				.taskDefinitionKey("UserTask_Watchout")
+				.processInstanceId(lProcessInstanceId)
+				.singleResult();
+		if (lTask != null)
+		{
+			String lTaskId = lTask.getId();
+			lResult += "&task=" + lTaskId;
+			this.logger.info(
+					"found task %s matching to proc inst id %s",
+					lTaskId,
+					lProcessInstanceId);
+		}
 		return lResult;
 	}
 }
