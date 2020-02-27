1b2cb157019879b5e2f5eebc5d10161a8c5ba04e
cjacobme <cjacobme@aol.com>
2020-02-27 19:22:48 +0100

    add a csv attachment

--- CJ  -  Experiments  -  Camunda Parent/10-send-mail/src/main/java/cj/software/experiments/camunda/_10_mail/util/MyMailSender.java
+++ CJ  -  Experiments  -  Camunda Parent/10-send-mail/src/main/java/cj/software/experiments/camunda/_10_mail/util/MyMailSender.java
@@ -1,10 +1,21 @@
 package cj.software.experiments.camunda._10_mail.util;
 
+import java.io.IOException;
+import java.io.StringWriter;
+import java.nio.charset.Charset;
+import java.time.OffsetDateTime;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,14 +27,39 @@ public class MyMailSender
 	private JavaMailSender javaMailSender;
 
 	public void sendMail(String pFrom, String pTo, String pSubject, String pContent)
+			throws MessagingException,
+			IOException
 	{
-		SimpleMailMessage lSimpleMailMessage = new SimpleMailMessage();
-		lSimpleMailMessage.setFrom(pFrom);
-		lSimpleMailMessage.setTo(pTo);
-		lSimpleMailMessage.setSubject(pSubject);
-		lSimpleMailMessage.setText(pContent);
+		MimeMessage lMimeMessage = this.javaMailSender.createMimeMessage();
+		MimeMessageHelper lHelper = new MimeMessageHelper(lMimeMessage, true, "UTF-8");
+		lHelper.setFrom(pFrom);
+		lHelper.setTo(pTo);
+		lHelper.setSubject(pSubject);
+		lHelper.setText(pContent);
+
+		String lCsvContents = this.createCsvContents();
+		ByteArrayResource lBAR = new ByteArrayResource(
+				lCsvContents.getBytes(Charset.forName("UTF-8")));
+		lHelper.addAttachment("data.csv", lBAR);
+
 		this.logger.info("send mail \"%s\" from %s to %s...", pSubject, pFrom, pTo);
-		this.javaMailSender.send(lSimpleMailMessage);
+		this.javaMailSender.send(lMimeMessage);
 		this.logger.info("mail was sent");
+
+	}
+
+	private String createCsvContents() throws IOException
+	{
+		try (StringWriter lSW = new StringWriter())
+		{
+			try (CSVPrinter lCsvPrinter = new CSVPrinter(lSW, CSVFormat.DEFAULT.withDelimiter(';')))
+			{
+				lCsvPrinter.printRecord("timestamp", OffsetDateTime.now());
+				lCsvPrinter.printRecord("value", 42);
+				lCsvPrinter.printRecord("remark", "Answer to all questions");
+			}
+			String lResult = lSW.toString();
+			return lResult;
+		}
 	}
 }
