4a1a3020519a787e5a127caee69966538f466b91
cjacobme <cjacobme@aol.com>
2020-03-21 18:30:25 +0100

    Start main process with a wait until input

--- CJ  -  Experiments  -  Camunda Parent/12-messages-receiving/src/main/java/cj/software/experimtens/camunda/_12_multiple_receives/MultiReceivesMain.java
+++ CJ  -  Experiments  -  Camunda Parent/12-messages-receiving/src/main/java/cj/software/experimtens/camunda/_12_multiple_receives/MultiReceivesMain.java
@@ -1,9 +1,11 @@
 package cj.software.experimtens.camunda._12_multiple_receives;
 
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableProcessApplication
 public class MultiReceivesMain
 {
 	public static void main(String[] pArgs)
