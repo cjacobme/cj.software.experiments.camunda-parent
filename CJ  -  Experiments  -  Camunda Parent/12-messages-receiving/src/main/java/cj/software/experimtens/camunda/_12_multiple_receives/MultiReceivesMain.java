6bd352c99e6819008bccac9e9f8cf91bf9dff7d3
cjacobme <cjacobme@aol.com>
2020-03-21 17:58:07 +0100

    setup new project

---
+++ CJ  -  Experiments  -  Camunda Parent/12-messages-receiving/src/main/java/cj/software/experimtens/camunda/_12_multiple_receives/MultiReceivesMain.java
@@ -0,0 +1,13 @@
+package cj.software.experimtens.camunda._12_multiple_receives;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiReceivesMain
+{
+	public static void main(String[] pArgs)
+	{
+		SpringApplication.run(MultiReceivesMain.class, pArgs);
+	}
+}
